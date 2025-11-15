1398c4a13acabe724946f5f0ecf502c87d292584
r220 <upontime000@gmail.com>
2025-11-15 06:26:28 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,321 @@
+from telegram import ReplyKeyboardMarkup, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes, MessageHandler, filters
+import json
+
+TOKEN = "7769385180:AAHKmXxeeHvRWW1SXIMdh_XZP0xpPaJ8Sjo"
+JSON = 'data.json'
+PAGE_SIZE = 10  # number of channels per page
+RETURN_BACK = "RETURN BACK"
+ADD = "ADD"
+DEL = "DELETE"
+file = ''
+request_add_word = ''   # 0: none, 1: allowed, 2: blocked
+input_state = None     # waiting input from user on replyMarkupButton
+KEYBOARDS = {
+    'category': {
+        'msg': "choose Category:",
+        'keyboard': InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("Users ⚙️", callback_data="users"),
+                InlineKeyboardButton("Words 📝", callback_data="words")
+            ],
+        ])
+    },
+    'users': {
+        'msg': "👥 Manage Users:",
+        'keyboard': InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("ACCEPTED", callback_data="allowed_users"),
+                InlineKeyboardButton("BLOCKED", callback_data="blocked_users"),
+            ],
+            [InlineKeyboardButton(RETURN_BACK, callback_data="category")]
+        ]),
+    },
+
+    'words': {
+        'msg': "💬 Manage Words:",
+        'keyboard': InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("ACCEPTED", callback_data="allowed_words"),
+                InlineKeyboardButton("BLOCKED", callback_data="blocked_words"),
+            ],
+            [InlineKeyboardButton(RETURN_BACK, callback_data="category")]
+        ]),
+    },
+
+    'allowed_users': {
+        'msg': "✅ Allowed Users - Add or Remove:",
+        'keyboard': InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton(ADD, callback_data="add-allowed_users"),
+                InlineKeyboardButton(DEL, callback_data="del-allowed_users"),
+            ],
+            [InlineKeyboardButton(RETURN_BACK, callback_data="users")]
+        ]),
+    },
+
+    'blocked_users': {
+        'msg': "🚫 Blocked Users - Add or Remove:",
+        'keyboard': InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton(ADD, callback_data="add-blocked_users"),
+                InlineKeyboardButton(DEL, callback_data="del-blocked_users"),
+            ],
+            [InlineKeyboardButton(RETURN_BACK, callback_data="users")]
+        ]),
+    },
+
+    'allowed_words': {
+        'msg': "✅ Allowed Words – Add or Remove:",
+        'keyboard': InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton(ADD, callback_data="add-allowed_words"),
+                InlineKeyboardButton(DEL, callback_data="del-allowed_words"),
+            ],
+            [InlineKeyboardButton(RETURN_BACK, callback_data="words")]
+        ]),
+    },
+
+    'blocked_words': {
+        'msg': "🚫 Blocked Words - Add or Remove:",
+        'keyboard': InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton(ADD, callback_data="add-blocked_words"),
+                InlineKeyboardButton(DEL, callback_data="del-blocked_words"),
+            ],
+            [InlineKeyboardButton(RETURN_BACK, callback_data="words")]
+        ]),
+    },
+    "add-allowed_users": {
+        'msg': "select user to allow forward messages:",
+        'list': "remained_users",
+        'prev': 'allowed_users'
+        },
+    "del-allowed_users": {
+        'msg': "select user to remove access:",
+        'list': "allowed_users",
+        'prev': 'allowed_users'
+        },
+    "add-blocked_users": {
+        'msg': "select user to block forward messages:",
+        'list': "remained_users",
+        'prev': 'blocked_users'
+        },
+    "del-blocked_users": {
+        'msg': "select blocked user to remove block:",
+        'list': "blocked_users",
+        'prev': 'blocked_users'
+        },
+    # add-allowed_words "reply with a message that containes the word:"
+    "del-allowed_words": {
+        'msg': "select allowed word that will be removed:",
+        'list': 'allowed_words',
+        'prev': 'allowed_words'
+
+        },
+    # add-blocked_words "reply with a message that containes the word:"
+    "del-blocked_words": {
+        'msg': "select word:",
+        'list': "blocked_words",
+        'prev': 'blocked_words'
+    }
+}
+file_lists = {
+    "remained_users": [],
+    "allowed_users": [],
+    "blocked_users": [],
+    "allowed_words": [],
+    "blocked_words": []
+}
+# -------------------------------------------------------------------
+def load_json():
+    global file
+    with open(JSON, "r") as f:
+        file = json.load(f)
+        # print(file)
+        file_lists["remained_users"] = file["users"]["remained"]
+        file_lists['allowed_users'] = file['users']['allowed']
+        file_lists["blocked_users"] = file["users"]["blocked"]
+        file_lists['allowed_words'] = file['words']['allowed']
+        file_lists["blocked_words"] = file["words"]["blocked"]
+        # return file
+# -------------------------------------------------------------------
+def get_dict_from_id(dicts, id):
+    # id = int(id)
+    # print(f'id: {id}','dicts: ', dicts)
+    for d in dicts:
+        if d.get("id") == id:
+            return d
+# -------------------------------------------------------------------
+async def choice_selected(choice: str, query):
+    # print(choice)
+    global file
+    # data = 'add:allowed_users--00000'
+    # ------------------------
+    command = choice[:3] # 'add'
+    split = choice[4:].split('-') # ['allowed_users', '00000'] / ['allowed_users', '', '00000']
+    category = split[0].split('_') # ['allowed', 'users']
+    c_list = category[0] # 'allowed'
+    c_name = category[1] # 'users'
+    data = ''
+    # ------------------------
+    msg = '' # msg sent to user in the end
+    # ------------------------
+    match c_name:
+        case 'users':
+            if len(split) == 3:
+                data = '-'.join(split[1:]) # '00000' / "-00000"
+            match command:
+                case 'add':
+                    # del from remained + add to c_list
+                    temp = get_dict_from_id(file[c_name]['remained'], data)
+                    if temp:
+                        file[c_name]['remained'].remove(temp)
+                        file[c_name][c_list].append(temp)
+                        msg = f'✅ user "{temp['name']}" added successfully !'
+                case 'del':
+                    # add to remained - then del
+                    temp = get_dict_from_id(file[c_name][c_list], data)
+                    print('temps: ', temp)
+                    if temp:
+                        file[c_name][c_list].remove(temp)
+                        file[c_name]['remained'].append(temp)
+                        msg = f'✅ user "{temp['name']}" removed successfully !'                    
+
+        case 'words':
+            data = ''.join(split[1:]) # '00000'
+            match command:
+                case 'add':
+                    if data not in file[c_name][c_list]:
+                        file[c_name][c_list].append(data)
+                        msg = f'✅ word "{data}" added successfully !'
+                case 'del':
+                    if data in file[c_name][c_list]:
+                        file[c_name][c_list].remove(data)
+                        msg = f'✅ word "{data}" removed successfully !'
+    
+    if (msg):            
+        with open(JSON, "w") as f:
+            json.dump(file, f, indent = 4)
+        load_json()
+        # await query.edit_message_text(msg, )
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(RETURN_BACK, callback_data=f'{split[0]}')]])
+        await query.edit_message_text(msg, reply_markup=reply_markup)
+
+    else:
+        await query.edit_message_text(f"❌ Request Failed..\nif you think that the problem is from bot, don't hesitate contacting dv.Reem ^_* !")
+        
+# -------------------------------------------------------------------  
+# ===== Helper: build channel menu with pagination =====
+def build_keyboard(data: str, page: int):
+    # data = 'add-allowed_users'
+    global file
+    cmd = data.split('-')[0]
+    block = KEYBOARDS[data]
+    list = file_lists[block['list']]
+    prev = block['prev']
+    start = page * PAGE_SIZE
+    end = start + PAGE_SIZE
+    cmdList = data.replace('-', ':')
+    buttons = []
+    
+    if (data.endswith('users')):
+        buttons = [
+            [InlineKeyboardButton(text=str(user.get("name")), callback_data=f"{cmdList}-{user['id']}")]
+            for user in list[start:end]
+        ]
+    elif (data.endswith('words')):
+        buttons = [[InlineKeyboardButton(name, callback_data=f"{cmdList}-{name}")] for i, name in enumerate(list[start:end], start)]
+
+    nav_buttons = []
+    if page > 0:
+        nav_buttons.append(InlineKeyboardButton("⬅️ Prev", callback_data=f"page_{page-1}_{data}"))
+    if end < len(list):
+        nav_buttons.append(InlineKeyboardButton("Next ➡️", callback_data=f"page_{page+1}_{data}"))
+    if nav_buttons:
+        buttons.append(nav_buttons)
+    buttons.append([InlineKeyboardButton(RETURN_BACK, callback_data=prev)])
+    
+    return InlineKeyboardMarkup(buttons)
+
+async def edit_message_text(data: str, query, page = 0):
+    msg = KEYBOARDS[data]["msg"]
+    keyboard = ''
+    if 'list' in KEYBOARDS[data]:
+        keyboard = build_keyboard(data, page)
+    else: 
+        keyboard = KEYBOARDS[data]["keyboard"]
+    await query.edit_message_text(msg, reply_markup=keyboard)
+        
+# ===== Handle all button presses =====
+async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    data = query.data
+    
+    # --------------------------------------------
+    # START
+    # --------------------------------------------
+    
+    # page (next - prev)
+    if data.startswith("page_"):
+        split = data.split("_")
+        page = int(split[1])
+        data = '_'.join(split[2:])
+        reply_markup = build_keyboard(data, page)
+        await query.edit_message_text(f"Select (page {page+1}):", reply_markup=reply_markup)
+    # choice selected
+    elif ':' in data.split('_')[0]:
+        await choice_selected(data, query)
+    # other choices
+    elif data in ['add-allowed_words', 'add-blocked_words']:
+        global request_add_word
+        request_add_word = 'allowed' if ('allowed' in data) else 'blocked'
+        reply_markup = ReplyKeyboardMarkup(keyboard = [["Start input"]], resize_keyboard=True)
+        await query.message.reply_text("Tap the button below 👇", reply_markup=reply_markup)
+    else:
+        await edit_message_text(data, query)
+    
+async def add_word_event(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global input_state, request_add_word
+    text = update.message.text
+    # -----------------------------
+    if text == "Start input":
+        # user pressed the ReplyKeyboardButton
+        input_state = "awaiting_input"
+        await update.message.reply_text("Now send your word:")
+    else:
+        # check if the user was supposed to send a reply
+        if input_state == "awaiting_input":
+            input_state = None
+            # save text to file
+            if request_add_word:
+                file['words'][request_add_word].append(text)
+                with open(JSON, "w") as f:
+                    json.dump(file, f, indent = 4)
+                load_json()
+                reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(RETURN_BACK, callback_data=f'{request_add_word}_words')]])
+                await update.message.reply_text("✅ word is saved.", reply_markup=reply_markup)
+        else:
+            await update.message.reply_text("🤖 send '/start' to use bot...")
+    
+
+async def show_category(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reply_markup = KEYBOARDS["category"]["keyboard"]
+    await update.message.reply_text(KEYBOARDS["category"]["msg"], reply_markup=reply_markup)
+
+# ===== Main =====
+def main():
+    load_json()
+    
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", show_category))
+    app.add_handler(CallbackQueryHandler(button_callback))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, add_word_event))
+    
+    print("Bot is running...")
+    # print('kk is ', file)
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,132 @@
+from telethon import TelegramClient, events
+from telethon.tl import types
+import json
+import logging
+from dotenv import load_dotenv
+from os import getenv
+import asyncio
+import re
+
+logging.basicConfig(format='[%(levelname) %(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
+load_dotenv()
+
+# TELEGRAM CLIENT SETUP
+api_id = int(getenv('API_KEY'))  
+api_hash = getenv('API_HASH')
+client = TelegramClient('anon', api_id, api_hash)
+forwarded_ch = int(getenv('FORWARDED_CHANNEL'))
+me = ''
+JSON = 'data.json'
+BOT = 7769385180
+# json file setup
+file = ''
+file_lists = {
+    "remained_users": [],
+    "allowed_users": [],
+    "blocked_users": [],
+    "allowed_words": [],
+    "blocked_words": []
+}
+def load_json():
+    global file
+    with open(JSON, 'r') as f:
+        file = json.load(f)
+        file_lists["remained_users"] = file["users"]["remained"]
+        file_lists['allowed_users'] = file['users']['allowed']
+        file_lists["blocked_users"] = file["users"]["blocked"]
+        file_lists['allowed_words'] = file['words']['allowed']
+        file_lists["blocked_words"] = file["words"]["blocked"]
+
+# PROGRAM main event
+# @client.on(events.NewMessage(incoming=True))
+@client.on(events.NewMessage(outgoing=True, pattern = re.compile(r"(" + "|".join(map(re.escape, file_lists["allowed_words"])) + r")", re.IGNORECASE)))
+async def my_event_handler(event):
+    load_json()
+    # CHECK users
+    chat_id = str(event.chat_id)
+    # 1 - allowed user
+    if chat_id in [d['id'] for d in file_lists['allowed_users']]:
+            await event.forward_to(forwarded_ch)
+            print("forwarded -- allowed id")
+            return
+    
+    # 2 - not allowed word
+    message = event.message.message
+    found_allowed_word = any(a_word in message for a_word in file_lists['allowed_words'])
+    if not found_allowed_word and (message != '/update'):
+        return
+    
+    else:
+        # 3 - blocked user
+        if chat_id in [d['id'] for d in file_lists['blocked_users']]:
+            print("not forwarded -- blocked id")
+            return
+        # 4 - blocked word
+        found_blocked_word = any(b_word in message for b_word in file_lists['blocked_words'])
+        if found_blocked_word:
+            print("not forwarded -- blocked word")
+            return
+        # 5 - neither user/word is blocked
+        if found_allowed_word:
+            await event.forward_to(forwarded_ch)
+            print("forwarded -- allowed word")
+            return  
+         
+        # extra (BOT): check added/removed channels/groups
+        if chat_id == BOT and message == '/update':
+            msgs = await refresh()
+            for i in range(0, len(msgs), 30):
+                await client.send_message(BOT, "\n".join(msgs[i:i+100]))
+            return
+
+# -----------------------------------------------------         
+# Detect when a chat/channel is deleted or inaccessible
+# ----------------------------------------------------- 
+async def refresh():
+    global file
+    dialogs = await client.get_dialogs()
+    
+    current = [
+        {
+            "id": str(d.id),
+            "name": d.entity.title
+        }
+        for d in dialogs
+        if isinstance(d.entity, (types.Chat, types.Channel))
+    ]
+    
+    s = file['users']
+    users = s['remained'] + s['allowed'] + s['blocked']
+    added = [c for c in current if c["id"] not in {u["id"] for u in users}]
+    removed = [u for u in users if u["id"] not in {c["id"] for c in current}]
+    msg = []
+    if not (added or removed):
+        msg = ["🤖 no changes to update.."]
+    else:
+        # ADDED
+        if added:
+            msg.append("🟢 You Joined to the following (channels/groups): \n")
+            for d in added:
+                file['users']['remained'].append(d)
+                msg.append(f"{d['name']}")
+        # REMOVED
+        if removed:
+            msg.append("\n🔴 You Left from the following (channels/groups): \n")
+            for d in removed:
+                if d["id"] in [i["id"] for i in s['allowed']]:
+                    print('removed from allowed')
+                    file['users']['allowed'].remove(d)
+                elif d["id"] in [i["id"] for i in s['blocked']]:
+                    file['users']['blocked'].remove(d)
+                elif d["id"] in [i["id"] for i in s['remained']]:
+                    file['users']['remained'].remove(d)
+                msg.append(f"{d['name']}")         
+
+        with open(JSON, "w") as f:
+            json.dump(file, f, indent = 4)
+        load_json()
+    return msg
+
+with client:
+    load_json()
+    client.run_until_disconnected()
